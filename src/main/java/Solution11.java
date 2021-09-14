b9e9f1f0cd52594033a441145c6a42ff5623b1f9
Almond64 <almond64@knights.ucf.edu>
2021-09-13 23:49:26 -0400

    Initial Commit Failed

---
+++ src/main/java/Solution11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //How am I supposed to do all this with a single output statement?
+        System.out.println("How many euros are you exchanging? ");
+        double euros = input.nextDouble();
+        System.out.println("What is the exchange rate? ");
+        double rate = input.nextDouble();
+        //calculate dollars rounded up to the nearest cent
+        double dollars = rate * euros;
+        System.out.printf("%.2f euros at an exchange rate of %.4f is %.2f U.S. dollars.\n", euros, rate, Math.ceil(dollars));
+    }
+}
